cb8a74ee42f3f9b163a5563941f3322228e517fa
YosukeMatsuki <ysk0321agu@gmail.com>
2017-05-01 14:31:10 +0000

    Used the resource generator for posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430084742) do
-  
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20170501141919) do
+
+  create_table "posts", force: :cascade do |t|
+    t.date     "date"
+    t.text     "rationale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,10 +38,7 @@ ActiveRecord::Schema.define(version: 20170430084742) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
-
-   
-
